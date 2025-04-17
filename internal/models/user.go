005bed1917d377becd5f07779533ea732d2bd6f8
prhdm <parhamhoushmand@outlook.com>
2025-04-18 02:55:14 +0330

    initial commit

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,15 +5,14 @@ import (
 )
 
 type User struct {
-	ID                uint    `gorm:"primary_key;auto_increment"`
-	Email             string  `gorm:"unique;not null"`
-	InstagramID       string  `gorm:"unique;not null"`
-	Name              string  `gorm:"not null"`
-	APIKey            string  `gorm:"unique;not null"`
-	Password          string  `gorm:"not null"`
-	EmailVerified     bool    `gorm:"default:false"`
-	VerificationToken string  `gorm:"unique"`
-	StripeCustomerID  *string `gorm:"default:null"`
+	ID                uint   `gorm:"primary_key;auto_increment"`
+	Email             string `gorm:"unique;not null"`
+	InstagramID       string `gorm:"unique;not null"`
+	Name              string `gorm:"not null"`
+	APIKey            string `gorm:"unique;not null"`
+	Password          string `gorm:"not null"`
+	EmailVerified     bool   `gorm:"default:false"`
+	VerificationToken string `gorm:"unique"`
 	CreatedAt         time.Time
 	UpdatedAt         time.Time
 }
@@ -24,10 +23,10 @@ type Payment struct {
 	Amount               float64 `gorm:"not null"`
 	Status               string  `gorm:"not null"`
 	Currency             string  `gorm:"not null"`
-	PayPalOrderID        string  `gorm:"unique"`
-	NowPaymentsPaymentID string  `gorm:"unique"`
-	ZarinpalAuthority    string  `gorm:"unique"`
-	ZarinpalRefID        string  `gorm:"unique"`
+	PayPalOrderID        *string `gorm:"default:null"`
+	NowPaymentsPaymentID *string `gorm:"default:null"`
+	ZarinpalAuthority    *string `gorm:"default:null"`
+	ZarinpalRefID        *string `gorm:"default:null"`
 	CreatedAt            time.Time
 	UpdatedAt            time.Time
 }
